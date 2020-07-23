584f11bb4fb712994248a2e812c99abe4a8dee87
Joshua Folorunsho <joshuafolorunsho01@gmail.com>
2020-07-23 17:13:18 +0100

    add resources

---
+++ assets/js/main.js
@@ -0,0 +1,5 @@
+const hamburger = document.querySelector('.hamburger');
+const header = document.querySelector('.nav-mobile')
+hamburger.addEventListener('click', () => {
+      header.classList.toggle('toggle');
+})
