4ca21ecf830dfdd170b16c722eda99a7fe483a05
Joshua Folorunsho <joshuafolorunsho01@gmail.com>
2020-07-29 15:40:27 +0100

    add asset files

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,17 @@
-const hamburger = document.querySelector('.hamburger');
-const header = document.querySelector('.nav-mobile')
-hamburger.addEventListener('click', () => {
-      header.classList.toggle('toggle');
-})
+const openSidebarBtn  = document.querySelector('#open-sidebar');
+     const closeSidebarBtn  = document.querySelector('#close-sidebar');
+     const sidebar_name  = document.querySelector('.side-bar__name');
+     const aside = document.querySelector('aside');
+     const body = document.body;
+     closeSidebarBtn.addEventListener('click', () => {
+      body.classList.toggle('collapse-sidebar');
+      closeSidebarBtn.classList.remove('d-md-block')
+      openSidebarBtn.classList.add('d-md-block')
+   });
+     openSidebarBtn.addEventListener('click', () => {
+      body.classList.toggle('collapse-sidebar');
+      closeSidebarBtn.classList.add('d-md-block');
+      openSidebarBtn.classList.remove('d-md-block')
+   })
+
+   
